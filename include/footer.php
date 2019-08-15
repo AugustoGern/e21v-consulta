3fc1c279ee9a8d3623431578164911cfe94a9112
Adriano <hentergostbr2018@gmail.com>
2019-08-15 17:18:41 -0300

    Modificação footer

--- include/footer.php
+++ include/footer.php
@@ -1,11 +1,14 @@
 <div class="footer p-0 m-0" style="background-color: rgb(10,68,161,0.90);">
 		<div class="row col-sm" style="text-align: center;">
 			<div class="container">	
+				<p class="text-white mt-3" style="text-decoration: underline;">Nossas Redes Sociais</p>
+				<a href="#"><i class="fab fa-instagram mt-2 p-2" style="background-color: rgb(200, 200, 200); border-radius: 20px;  width: 20%;  color: black;"></i></a>
+				<a href="#"><i class="fab fa-facebook-f mt-2 p-2" style="background-color: rgb(200, 200, 200); border-radius: 20px;  width: 20%;  color: black;"></i></a>
+				<a href="#"><i class="fas fa-at mt-2 p-2" style="background-color: rgb(200, 200, 200); border-radius: 20px;  width: 20%;  color: black;"></i></a>
+				<hr style="color: red	">
+				<p class="text-white mt-3" style="text-decoration: underline;"><i>Direitos Reservedos a equipe do projeto TCC Entra21.</i></p>
 
-				<p class=" text-light col-sm text-center pr-0 mt-3"><strong class="text-success">NomeLogo </strong>© 2019</p>
-				
-				<p class="text-light "><i>Uma empresa Brasileira com muito orgulho.</i></p>
-
+					<p class=" text-light col-sm text-center pr-0 mt-3"><strong class="text-success">NomeLogo </strong>© 2019	</p>
 			</div>
 		</div>
 	</div>
