1a1f32620b84dfbfee89feb0bec9ae1f412adcaa
Adriano <hentergostbr2018@gmail.com>
2019-08-15 16:05:01 -0300

    Alteração sobre.php

--- sobre.php
+++ sobre.php
@@ -5,10 +5,10 @@
 	<title>Sobre a Página</title>
 	<script src="https://kit.fontawesome.com/3b47dd4ac9.js"></script>
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-	<link rel="stylesheet" type="text/css" href="media/css/estilos.css">
+	<link rel="stylesheet" type="text/css" href="media/css/index.css">
 </head>
-<body style="background-image: url(media/images/mdc-18.jpg); background-size: 100%; background-attachment: fixed; top: 1;">
 
+<body style="background-image: url(media/images/mdc-18.jpg); background-size: 100%; background-attachment: fixed; top: 1;">
 
 	<!-- Inclusão do HEADER -->
 	<?php require_once('include/header.php'); ?>
@@ -17,41 +17,43 @@
 
 	<!-- fundo da Página  -->
 
-		<div class="container mt-5 pt-4 mb-5 pb-4" style="text-align: center; background-image: linear-gradient( white, rgb(88, 148, 245)); border-radius: 15px"> 
-			<h1 style="color: black; font-family: Times New Roman";>Aqui você sabera tudo sobre nós!.</h1>
-			<br><br>
-			<h4 style="text-align: center; font-family: Times New Roman" class="ml-5">Seja bem vindo ao sobre!, Aqui você sabera tudo o que foi desenvolvido em nosso site e o que nossa página web!.</h4>
-	<div class="row">
-			<div class="col-4">
-					<img src="media/images/mdc-20.jpg" class="m-4 " style="border-radius: 50px; box-shadow: 0px 0px 0px 3px">
+	<div class="container mt-5 pt-4 mb-5 pb-4" style="text-align: center; background-image: linear-gradient( white, rgb(88, 148, 245)); border-radius: 15px;box-shadow: 5px 5px 5px rgba(0,0,0,0.5);"> 
+
+		<h1 style="color: black;text-decoration: underline;">Aqui você sabera tudo sobre nossa página e nossa política de qualidade e muito mais!.</h1>
+		<br>
+
+		<h4 style="text-align: center; font-family: Times New Roman" class="ml-5">A página foi desenvolvida por uma equipe de 4 Alunos do Entra21, Edição de 2019. O “nome do site " foi monitorada e auxiliada por nosso instrutor/professor João de Paula.</h4>
+		<hr>
+
+		<div class="row">
+			<div class="col-4 mt-3">
+				<img src="media/images/mdc-20.jpg" class="m-4 " style="border-radius: 50px;box-shadow: 5px 5px 5px rgba(0,0,0,0.5);">
 			</div>
-			<div class="col-6">
-				<h4 class="m-4 p-0" style="text-align: left; font-family: Times New Roman">hasjashj</h4>
+
+			<div class="col-8 mt-3">
+				<h4 class="m-4 p-0" style="text-align: left; font-family: Times New Roman"> A página vem se desenvolvendo a cada dia e com ela, vem o nosso objetivo de entregar um 'processo de atendimento ao cliente e com agilidade em seu procedimento de consulta ou pesquisa de sintomas "possíveis"'. Nossa equipe visa entregar o máximo de conforto e simplicidade aos nossos clientes e tendo com o site o mais importante que é entregar um profissional na área hospitalar que possa cuidar do paciente e auxilia-lo em sua caminhada ao bem-estar.</h4>
+			</div>
+
+			<div class="col-5	">
+				<h2 class="mt-4 " style="text-align: left; text-decoration: underline;"><li>Missão</li></h2>
+				<h6 class="ml-5" style="text-align: left;">Cuidar dos nossos pacientes e usuários, promovendo agilidade e saúde com todo conforto e segurança, empatia e eficiência.</h6>
+				<h2 class="mt-2 " style="text-align: left; text-decoration: underline;"><li>Política de Qualidade</li></h2>
+				<h6 class="ml-5" style="text-align: left;">Buscar sempre a satisfazer os clientes da melhor forma, prestando serviços de qualidade e acessibilidade.</h6>
+				<h2 class="mt-2 " style="text-align: left; text-decoration: underline;"><li>Visão</li></h2>
+				<h6>Ter uma ótima clientela e satisfazer nosso acessores.</h6>
+			</div>
+
+			<div class="col-7">
+				<img src="media/images/mdc.21.jpg" class="m-4" style=" border-radius: 50px;box-shadow: 5px 5px 5px rgba(0,0,0,0.5)">	
 			</div>
 		</div>
 	</div>
 
-			
+	<!-- rodapé -->
+	<?php require_once('include/footer.php'); ?>
 
-		</div>
-
-		<!-- rodapé  -->
-		<div class="footer p-0 m-0 pt-3 pb-2" style="background-color: rgb(10,68,161,0.90);">
-			<div class="row col-sm" style="text-align: center;">
-				<div class="container">	
-
-					<p class=" text-light col-sm text-center pr-0"><strong class="text-success">NomeLogo </strong>© 2019</p>
-
-					<p class="text-light "><i>Uma empresa Brasileira com muito orgulho.</i></p>
-
-				</div>
-			</div>
-		</div>
-
-
-
-		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-	</body>
-	</html>
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
