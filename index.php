905c422ddb05b83138f47f3762574dae1f8f234d
Thiago Lacerda <lacerdathiagosilva.99@gmail.com>
2019-08-16 15:11:20 -0300

    Alteração pagina index

--- index.php
+++ index.php
@@ -21,15 +21,43 @@
 
 	<!-- botão -->
 	<div class="text-center ">
-		<a href="consulta.php" class="btn btn-success text-center" style="margin-top: -75px;"> Realize sua consulta </a>
+		<a href="consulta.php" class="btn btn-success text-center" style="margin-top: -125px;"> Realize sua consulta </a>
 	</div>
+	<!-- =========== -->
+
+
+
+	<div class="container-fluid">
+		<div class="container">
+			<div class="card">
+				<div class="card-body">
+					fhdsfbdsiboi
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 	<!-- ==========================================FOOTER=========================================================== -->
 
 	<?php require_once('include/footer.php'); ?>
 	
 	
-
+	<!-- =========================================SCRIPT============================== -->
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
