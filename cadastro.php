29239552f9412274f88bdfac55d647b936a71339
Augusto Gern <augustogern@outlook.com>
2019-08-19 17:13:15 -0300

    JavaScript

--- cadastro.php
+++ cadastro.php
@@ -74,7 +74,7 @@
 						<label for="confSenha">Confirmar Senha: <strong class="text-danger"> *</strong></label>
 						<input type="password" name="confSenha" id="confSenha" class="form-control"><br>
 
-						<input type="submit" name="cadastro" id="cadastro" class="btn btn-primary form-control" value="Cadastre-se">
+						<input type="submit" name="cadastro" id="cadastro" class="btn btn-primary form-control" value="Cadastre-se" disabled="">
 					</form>
 				</div>
 			</div>
@@ -84,9 +84,11 @@
 	<!-- FOOTER -->
 	<?php require_once('include/footer.php'); ?>
 
+
 	<!-- BOOTSTRAP -->
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+	<script src="media/js/cadastro.js"></script>
 </body>
 </html>
