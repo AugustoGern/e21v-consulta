3f5bf74aeab3f3ddee49655302440037345b77ea
Adriano Serpa <HenterGostBR2018@gmail.com>
2019-08-27 17:20:59 -0300

    Alterações sobre.php

--- sobre.php
+++ sobre.php
@@ -7,9 +7,10 @@
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 	<link rel="stylesheet" type="text/css" href="media/css/index.css">
 	<link rel="stylesheet" type="text/css" href="media/css/cadastro.css">
+	<link href="https://fonts.googleapis.com/css?family=Montserrat:900,600&display=swap" rel="stylesheet">
 </head>
 
-<body>
+<body style="background-color: rgb(218, 235, 240);">
 
 	<!-- Inclusão do HEADER -->
 	<?php require_once('include/header.php'); ?>
@@ -25,30 +26,30 @@
 
 	<!-- fundo da Página  -->
 
-	<div class="container  pt-4 mb-5 pb-4 col-10 " style="text-align: center; background-image: linear-gradient( white, rgb(88, 148, 245)); border-radius: 15px;box-shadow: 5px 5px 5px rgba(0,0,0,0.5); margin-top: -150px;" > 
+	<div class="container  pt-4 mb-5 pb-4 col-10 " style="text-align: center; background-image: linear-gradient( white, rgb(100, 155, 200)); border-radius: 15px;box-shadow: 5px 5px 5px rgba(0,0,0,0.5); margin-top: -150px;" > 
 
-		<h1 style="color: black;text-decoration: underline;">Aqui você sabera tudo sobre nossa página e nossa política de qualidade e muito mais!.</h1>
+		<h1 style="color:rgb(89, 97, 105);font-family: 'Montserrat',sans-serif; font-weight: 600">Aqui você sabera tudo sobre nossa página e nossa política de qualidade e muito mais!</h1>
 		<br>
 
-		<h4 style="text-align: center; font-family: Times New Roman" class="p-4">A página foi desenvolvida por uma equipe de 4 Alunos do Entra21, Edição de 2019. O “nome do site " foi monitorada e auxiliada por nosso instrutor/professor João de Paula.</h4>
+		<h4 style="color: rgb(110, 120, 130); ,text-align: center; font-family: Times New Roman" class="p-4">A página foi desenvolvida por uma equipe de 4 Alunos do Entra21, Edição de 2019. O “nome do site " foi monitorada e auxiliada por nosso instrutor/professor João de Paula.</h4>
 		<hr>
 
 		<div class="row">
 			<div class="col-lg-4 col-md-5  mt-3">
-				<img src="media/images/mdc-20.jpg" class="m-4 col-10 d-none d-sm-block" style="border-radius: 50px;box-shadow: 5px 5px 5px rgba(0,0,0,0.5);">
+				<img src="media/images/mdc-20.jpg" class="m-4 col-10 d-none d-sm-block" style="border-radius: 40px;box-shadow: 5px 5px 5px rgba(0,0,0,0.5);">
 			</div>
 
 			<div class="col-md-12 col-lg-8 mt-3">
-				<h4 class="m-4 p-0" style="text-align: left; font-family: Times New Roman"> A página vem se desenvolvendo a cada dia e com ela, vem o nosso objetivo de entregar um 'processo de atendimento ao cliente e com agilidade em seu procedimento de consulta ou pesquisa de sintomas "possíveis"'. Nossa equipe visa entregar o máximo de conforto e simplicidade aos nossos clientes e tendo com o site o mais importante que é entregar um profissional na área hospitalar que possa cuidar do paciente e auxilia-lo em sua caminhada ao bem-estar.</h4>
+				<h4 class="m-4 p-0" style="text-align: left; font-family: Times New Roman;color: rgb(79, 87, 89);"> A página vem se desenvolvendo a cada dia e com ela, vem o nosso objetivo de entregar um 'processo de atendimento ao cliente e com agilidade em seu procedimento de consulta ou pesquisa de sintomas "possíveis"'. Nossa equipe visa entregar o máximo de conforto e simplicidade aos nossos clientes e tendo com o site o mais importante que é entregar um profissional na área hospitalar que possa cuidar do paciente e auxilia-lo em sua caminhada ao bem-estar.</h4>
 			</div>
 
 			<div class="col-md-5 col-12 ">
-				<h2 class="mt-4 " style="text-align: left; text-decoration: underline;"><li>Missão</li></h2>
-				<h6 class="ml-5" style="text-align: left;">Cuidar dos nossos pacientes e usuários, promovendo agilidade e saúde com todo conforto e segurança, empatia e eficiência.</h6>
-				<h2 class="mt-2 " style="text-align: left; text-decoration: underline;"><li>Política de Qualidade</li></h2>
-				<h6 class="ml-5" style="text-align: left;">Buscar sempre a satisfazer os clientes da melhor forma, prestando serviços de qualidade e acessibilidade.</h6>
-				<h2 class="mt-2 " style="text-align: left; text-decoration: underline;"><li>Visão</li></h2>
-				<h6>Ter uma ótima clientela e satisfazer nosso acessores.</h6>
+				<h2 class="mt-4 " style="text-align: left;color: rgb(82, 90, 100);font-family: 'Montserrat', sans-serif;"><li>Missão</li></h2>
+				<h4 class="ml-5" style="text-align: left;color: rgb(70, 72, 80);font-family: Times New Roman;">Cuidar dos nossos pacientes e usuários, promovendo agilidade e saúde com todo conforto e segurança, empatia e eficiência.</h4>
+				<h2 class="mt-2 " style="text-align: left;color: rgb(82, 90, 100);font-family: 'Montserrat', sans-serif; "><li>Política de Qualidade</li></h2>
+				<h4 class="ml-5" style="text-align: left;color: rgb(70, 72, 80);font-family: Times New Roman;">Buscar sempre a satisfazer os clientes da melhor forma, prestando serviços de qualidade e acessibilidade.</h4>
+				<h2 class="mt-2 " style="text-align: left;color: rgb(82, 90, 100);font-family: 'Montserrat', sans-serif; "><li>Visão</li></h2>
+				<h4 class="ml-5" style="text-align: left;color: rgb(70, 72, 80);font-family: Times New Roman;">Ter uma ótima clientela e satisfazer nosso acessores.</h4>
 			</div>
 
 			<div class="col-lg-7 ">
