715463a769571c48e74c8c98e585bc207ed2af0e
Adriano serpa <HenterGostBR2018gmail.com>
2019-08-27 14:13:03 -0300

    atualização footer

--- include/footer.php
+++ include/footer.php
@@ -2,9 +2,9 @@
 		<div class="row col-sm" style="text-align: center;">
 			<div class="container">	
 				<p class="text-white mt-3" style="text-decoration: underline;">Nossas Redes Sociais</p>
-				<a href="#"><i class="fab fa-instagram mt-2 p-2" style="background-color: rgb(200, 200, 200); border-radius: 20px;  width: 20%;  color: black;"></i></a>
-				<a href="#"><i class="fab fa-facebook-f mt-2 p-2" style="background-color: rgb(200, 200, 200); border-radius: 20px;  width: 20%;  color: black;"></i></a>
-				<a href="#"><i class="fas fa-at mt-2 p-2" style="background-color: rgb(200, 200, 200); border-radius: 20px;  width: 20%;  color: black;"></i></a>
+				<a href="https://www.instagram.com/accounts/login/?hl=pt-br"><i class="fab fa-instagram mt-2 p-2" style="background-color: rgb(200, 200, 200); border-radius: 20px;  width: 20%;  color: black;"></i></a>
+				<a href="https://pt-br.facebook.com/"><i class="fab fa-facebook-f mt-2 p-2" style="background-color: rgb(200, 200, 200); border-radius: 20px;  width: 20%;  color: black;"></i></a>
+				<a href="https://accounts.google.com/signin/v2/identifier?service=mail&continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&flowName=GlifWebSignIn&flowEntry=ServiceLogin#__utma=29003808.1180046663.1566925480.1566925480.1566925480.1&__utmb=29003808.0.10.1566925480&__utmc=29003808&__utmx=-&__utmz=29003808.1566925480.1.1.utmcsr=google%7Cutmccn=(organic)%7Cutmcmd=organic%7Cutmctr=(not%20provided)&__utmv=-&__utmk=80740692"><i class="fas fa-at mt-2 p-2" style="background-color: rgb(200, 200, 200); border-radius: 20px;  width: 20%;  color: black;"></i></a>
 				<hr style="color: red	">
 				<p class="text-white mt-3" style="text-decoration: underline;"><i>Direitos Reservedos a equipe do projeto TCC Entra21.</i></p>
 
