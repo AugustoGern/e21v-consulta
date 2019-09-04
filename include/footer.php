10a2d3b1ab6c6aab566dab718c1fec52be9a1b0c
Thiago Lacerda <lacerdathiagosilva.99@gmail.com>
2019-09-04 16:32:43 -0300

    footer

--- include/footer.php
+++ include/footer.php
@@ -3,65 +3,73 @@
 
 <footer class="ft page-footer font-small indigo pt-3">
 
-    <!-- Footer Links -->
-    <div class="container text-center text-md-left">
+  <!-- Footer Links -->
+  <div class="container text-center text-md-left">
 
-      <!-- Grid row -->
-      <div class="row">
+    <!-- Grid row -->
+    <div class="row">
 
-        <!-- Grid column -->
-        <div class="col-md-2 mx-auto">
+      <!-- Grid column -->
+      <div class="col-md-2 mx-auto">
 
-          <!-- Links -->
-          <h4 class="font-weight-bold text-uppercase mt-4 mb-4 text-white">Redes Sociais</h4>
-          <ul class="nt list-unstyled">
-            <li class="">
-              <i class="fab fa-facebook " ></i>
-              <a href="https://pt-br.facebook.com/pages/category/hospital/?page=4">Facebook </a>
-            </li>
-            <li>
-              <i class="fab fa-instagram "></i>
-              <a href="https://www.instagram.com/hospitalmaededeus/">Instagram</a>
-            </li>
-            <li>
-              <i class="fab f fa-twitter "></i>
-              <a href="https://twitter.com/hashtag/hospital">twitter</a>
-            </li>
-            <li>
-              <i class="fas fa-at" ></i>
-              <a href="https://gmail.com/mail/help/intl/pt_pt/about.html  ">Nosso Email</a>
-            </li>
-          </ul>
-        </div>
+        <!-- Links -->
+        <h4 class="font-weight-bold text-uppercase mt-4 mb-4 text-white">Redes Sociais</h4>
+        <ul class="nt list-unstyled">
+          <li class="">
+            <i class="fab fa-facebook " ></i>
+            <a class="text-white" href="https://pt-br.facebook.com/pages/category/hospital/?page=4"
+            style="text-decoration: none;">Facebook </a>
+          </li>
 
-        <!-- Grid column -->
-        <div class="col-md-2 mx-auto">
+          <li>
+            <i class="fab fa-instagram "></i>
+            <a  class="text-white" href="https://www.instagram.com/hospitalmaededeus/"
+            style="text-decoration: none;">Instagram</a>
+          </li>
 
-          <!-- Links -->
-          <h4 class="font-weight-bold text-uppercase mt-4 mb-4 text-white">Usuário</h4>
-          <ul class="nt list-unstyled">
-            <li>Reportar erros</li>
-            <li>Dúvidas</li>
-            <li>Sugestões</li>
-            <li>Feedback</li>
-          </ul>
-        </div>
+          <li>
+            <i class="fab f fa-twitter "></i>
+            <a class="text-white" href="https://twitter.com/hashtag/hospital" 
+            style="text-decoration: none;">twitter</a>
+          </li>
 
-        <div class="col-md-6 mt-md-0 mt-3">
+          <li>
+            <i class="fas fa-at" ></i>
+            <a class="text-white" href="https://gmail.com/mail/help/intl/pt_pt/about.html" 
+            style="text-decoration: none;">Nosso Email</a>
+          </li>
 
-          <!-- Content -->
-          <h4 class="text-uppercase font-weight-bold mt-4 mb-4 text-white">Observações</h4>
-          <p class="text-white">Toda a Página está em desenvolvimento, por uma equipe de jovens do programa Entra 21 Edição 2019. E esta sendo desenvolvida com muito orgulho. Temos como objetivo agradar nossos clientes. Se possível deixe seu feedback e Sugestões ou até mesmo algo que gostaria de ver em nossa página.</p>
-        </div>
+        </ul>
+      </div>
+
+      <!-- Grid column -->
+      <div class="col-md-2 mx-auto">
+
+        <!-- Links -->
+        <h4 class="font-weight-bold text-uppercase mt-4 mb-4 text-white">Usuário</h4>
+        <ul class="nt list-unstyled">
+          <li>Reportar erros</li>
+          <li>Dúvidas</li>
+          <li>Sugestões</li>
+          <li>Feedback</li>
+        </ul>
+      </div>
+
+      <div class="col-md-6 mt-md-0 mt-3">
+
+        <!-- Content -->
+        <h4 class="text-uppercase font-weight-bold mt-4 mb-4 text-white">Observações</h4>
+        <p class="text-white">Toda a Página está em desenvolvimento, por uma equipe de jovens do programa Entra 21 Edição 2019. E esta sendo desenvolvida com muito orgulho. Temos como objetivo agradar nossos clientes. Se possível deixe seu feedback e Sugestões ou até mesmo algo que gostaria de ver em nossa página.</p>
       </div>
     </div>
+  </div>
 
-    <!-- Copyright -->
-    <div class="ft1 footer-copyright text-center pt-4 pb-2 text-white">
-     <p>Direitos Reservedos a equipe do projeto TCC Entra21.
-       <strong > NomeLogo © 2019</strong>  
-     <a class="pr-2 text-white" style="float: right; color: black;" href="painel.php">Administrador</a>
-     </p>
+  <!-- Copyright -->
+  <div class="ft1 footer-copyright text-center pt-4 pb-2 text-white">
+   <p>Direitos Reservedos a equipe do projeto TCC Entra21.
+     <strong > SymDinder © 2019</strong>  
+     <a class="pr-3 text-white" style="float: right; color: black;" href="painel.php">Administrador</a>
+   </p>
 
-   </div>
- </footer>
+ </div>
+</footer>
