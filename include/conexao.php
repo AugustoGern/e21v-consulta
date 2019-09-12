01130b84b8d9dadb3c2da9860eb2dd0f9fb43c88
Adriano Serpa <HenterGostBR2018@gmail.com>
2019-09-12 15:48:38 -0300

    aaaa

--- include/conexao.php
+++ include/conexao.php
@@ -1,15 +1,15 @@
 <?php
-	
+
 	CONST HOST = "faqhospital.tk:3306";
 	CONST USER = "faqhospi";
 	CONST PASS = "entra21@Blusoft";
 	CONST DB = "faqhospi_master";
 
-	$con = mysqli_connect(HOST, USER, PASS, DB);
+$con = mysqli_connect(HOST, USER, PASS, DB);
 
-	if (!$con) {
-		die("ERRO: Não foi possível conectar ao banco de dados ->" . mysqli_connect_erro());
-	}
+if (!$con) {
+	die("ERRO: Não foi possível conectar ao banco de dados ->" . mysqli_connect_erro());
+}
 
 
 
