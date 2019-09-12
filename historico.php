e274c0ab3811a02db526fc9b6ff00da6fc608b86
Thiago Lacerda <lacerdathiagosilva.99@gmail.com>
2019-09-12 17:24:04 -0300

    historico

--- historico.php
+++ historico.php
@@ -1,5 +1,5 @@
 <?php
-	require_once('include/paginas_restritas.php');
+require_once('include/paginas_restritas.php');
 ?>
 
 <!DOCTYPE html>
@@ -10,7 +10,10 @@
 	<script src="https://kit.fontawesome.com/3b47dd4ac9.js"></script>
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 	<link rel="stylesheet" type="text/css" href="media/css/index.css">
+	<link rel="stylesheet" type="text/css" href="media/css/media.css">
+	<link rel="stylesheet" type="text/css" href="media/css/sobre.css">
 	<link rel="shortcut icon" type="image/x-icon" href="media/images/4232logo__.ico">
+	<link href="https://fonts.googleapis.com/css?family=Montserrat:900,600&display=swap" rel="stylesheet">
 </head>
 <body >
 	<?php require_once('include/header.php'); ?>
@@ -18,7 +21,7 @@
 	
 
 	<!-- =========================================================================== -->
-	<div style="background-image: url('media/images/mdc-18.jpg'); min-height: 360px; min-width: 100%; background-size: 100%; background-position: center center; padding-top: 128px; margin-top: -154px; background-repeat: no-repeat;" class="shadow">
+	<div class="shadow img_sobre" style="background-image: url('media/images/fundo.jpg');  min-height: 320px; min-width: 100%; background-size: 100%; background-position: center center; padding-top: 128px; margin-top: -184px; background-repeat: no-repeat;"">
 		<div class="text-center my-5">
 			
 		</div> 
@@ -26,14 +29,14 @@
 	<!-- ==============================================CONTEUDO==================================================== -->
 
 	<div class="container-fluid">
-		<div class="container"  style="font-family: 'Montserrat', sans-serif; margin-top: -200px; ">
+		<div class="container pb-5"  style=" margin-top: -200px; ">
 
-			<h1 class="text-center">Seu histórico de consultas:</h1>
+			<h1 id="batata">Seu histórico de consultas:</h1>
 			
-			<div class="card mb-5">
+			<div class="card mt-5">
 				<div class="card-body">
 					<div class="row col-12 col-md">
-				
+
 
 						<table class="table table-hover table-striped m-0">
 							<tbody>
@@ -43,11 +46,10 @@
 								</tr>
 								<?php } ?>
 							</tbody>
+							<a href="index.php" class="btn btn-success text-center"> Voltar </a>
 						</table>
-						<a href="index.php" class="btn btn-success ">Voltar</a>
-
-
 					</div>
+
 					
 				</div>
 			</div>
