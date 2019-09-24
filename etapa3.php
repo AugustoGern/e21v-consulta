f4e12f93479a2650d5849eb93cd7ce1dc29cc22c
thiagoo918 <lacerdathiagosilva.99@gmail.com>
2019-09-24 19:42:58 -0300

    nada

--- etapa3.php
+++ etapa3.php
@@ -1,5 +1,5 @@
 <?php 
-	require_once('include/paginas_restritas.php');
+require_once('include/paginas_restritas.php');
 ?>
 
 <!DOCTYPE html>
@@ -18,59 +18,58 @@
 	
 	<!-- HEADER -->
 	<?php 	
-		require_once('include/header.php');
-		require_once('backend/etapa2_backend.php');
-		require_once('backend/etapa3_backend.php');
-		//var_dump($_SESSION['resp_selec']);
-		// echo "<pre>";
-		// // var_dump($pesos);
-		// echo "<br>";
-		// // var_dump($tipo);
-		// echo "<br>";
+	require_once('include/header.php');
+	require_once('backend/etapa2_backend.php');
+	require_once('backend/etapa3_backend.php');
 	?>
 
 	<div class="container-fluid">
-		<div class="container">
+		<div class="container mb-5 pb-5">
 			<div class="row">		
 				<h2>De acordo com as informações coletadas nas etapas anteriores, acreditamos que para o seu bem-estar você deveria marcar uma consulta com um desses profissionais:</h2>
-				<!-- <hr  style="border: 0; margin-top: 30px; height: 3px; width: 700px; background-image: linear-gradient(to right, rgba(0, 0, 0, 0), rgba(34, 86, 170, 0.75), rgba(0, 0, 0, 0));"> -->
 			</div>
+
 			<?php while ($resultadoMedicos = mysqli_fetch_array($queryMedicos)) { ?>
-				<hr style="background-color: rgb(168, 182, 186); width: 1000px; margin-top:50px; height: 1px; ">
-			<div class="media mt-4 mb-5">
- 			 	<div class="media-body pl-5">
-    				<h5 class="mt-0 mb-3">Informações do médico:</h5>
-    					<li>
-    						<i class="fas fa-user mr-2"></i>
-    					     <?=$resultadoMedicos['nome']?>
-    					</li>
+			<hr style="background-color: rgb(168, 182, 186); width: 1000px; margin-top:50px; height: 1px; ">
+			<div class="media mt-4 ">
+				<div class="media-body pl-5">
+					<div class="ml-5">
+						<h5>Informações do médico:</h5>
+						<li>
+							<i class="fas fa-user mt-1 mr-2"></i>
+							<strong>Nome: </strong><?=$resultadoMedicos['nome']?>
+						</li>
 
-    					<li class="mt-2">
-    						<i class="fas fa-user-md mr-2"></i>
-    						<?=$resultadoMedicos['area']?>
-    					</li>
+						<li class="mt-1">
+							<i class="fas fa-user-md mr-2"></i>
+							<strong>Especialidade: </strong><?=$resultadoMedicos['area']?>
+						</li>
 
-    					<li class="mt-2">
-    						<i class="fas fa-hospital mr-2"></i>
-    						<?=$resultadoMedicos['hospital']?>
-    					</li>
+						<li class="mt-1">
+							<i class="fas fa-hospital mr-2"></i>
+							<strong>Local de Atuação: </strong><?=$resultadoMedicos['hospital']?>
+						</li>
 
-    					<li class="mt-2">
-    						<i class="fas fa-at mr-2"></i>
-    						<?=$resultadoMedicos['email']?>
-    					</li>
+						<li class="mt-1">
+							<i class="fas fa-at mr-2"></i>
+							<strong>E-mail: </strong><?=$resultadoMedicos['email']?>
+						</li>
 
-    					<li class="mt-2">
-    						<i class="fas fa-phone-alt mr-2"></i>
-    						<?=$resultadoMedicos['telefone']?>
-    					</li>
-  				</div>
-  			<img  src="media/images/mdc-20.jpg" class="img-fluid img_medico">
+						<li class="mt-1">
+							<i class="fas fa-phone-alt mr-2"></i>
+							<strong>Telefone: </strong><?=$resultadoMedicos['telefone']?>
+						</li>
+						<a href="medico.php" class="btn btn-secondary mt-3">Mais informações</a>
+
+					</div>
+				</div>
+				<div class="mr-5 mt-2">
+					<img  src="media/images/mdc-20.jpg" class="img-fluid img_medico_etp3">
+				</div>
+			</div>	
+			<?php } ?>
 		</div>
-
-	<?php } ?>
 	</div>
-</div>
 
 	<!-- footer -->
 	<?php require_once('include/footer.php'); ?>
