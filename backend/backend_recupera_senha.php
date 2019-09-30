b3e0651f3c33b2038488d65eff3f4f6ea8c369b0
Vinicios Pinheiro <vinicios.pinheiro2016@gmail.com>
2019-09-30 17:18:51 -0300

    nada aaa

--- backend/backend_recupera_senha.php
+++ backend/backend_recupera_senha.php
@@ -1,11 +1,35 @@
 <?php
 require_once('include/conexao.php');
+require_once('backend/funcoes.php');
 
 
-if (isset($_GET['ok'])) {
-	$token = substr(md5(time()), 0, 6);
+
+if (isset($_POST['ok'])) {
+
+	$email = $_POST['email']);
+	$senha = $_POST['senha']);
+
+if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
 }
-echo substr(md5(time()), 0, 6);
+
+$sqlLogin = "SELECT * FROM tb_usuario WHERE senha = '$senha'";
+$queryLogin = mysqli_query($con, $sqlLogin);
+$resultadoLogin = mysqli_fetch_array($queryLogin);
 
 
+
+$token = substr(md5(time()), 0, 6);
+$novasenha = md5(md5($token));
+
+if (mail($email, "Sua nova senha", "Sua nova senha".$token)){
+
+
+	$sqlLogin = "UPDATE tb_usuario SET token_recuperacao = '$novasenha' WHERE  email = '$email'";
+	$queryLogin = mysqli_query($con, $sqlLogin);
+
+}
+
+
+
+}
 ?>
