a6883d380d70d72cf8777cb9bacf78e6aae6aa32
thiagoo918 <lacerdathiagosilva.99@gmail.com>
2019-10-01 19:04:39 -0300

    kldkhfsdiovhd

--- painel.php
+++ painel.php
@@ -15,7 +15,7 @@
 
 <body>
   <nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-    <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">Hospital Entra21</a>
+    <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">SymFinder</a>
     <ul class="navbar-nav px-3">
       <li class="nav-item text-nowrap d-none d-sm-block">
         <a class="nav-link" href="index.php">Sair</a>
