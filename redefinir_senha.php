0f68fb8e3c4cfecbaf2f5a6d6dcd654291227e14
Augusto Gern <augustogern@outlook.com>
2019-10-02 17:26:36 -0300

    foiii krll

--- redefinir_senha.php
+++ redefinir_senha.php
@@ -1,12 +1,15 @@
+
 <?php 
 	require_once('backend/backend_recupera_senha.php');
 ?>
+
+
 
 <!DOCTYPE html>
 <html>
 <head>
 	<meta charset="utf-8">
-	<title>Recuperar Senha</title>
+	<title>Redefinir Senha</title>
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 	<link rel="stylesheet" type="text/css" href="media/css/nova_senha.css">
 </head>
@@ -17,13 +20,16 @@
 	<div class="container pt-5 text-center">
 		<form class="" method="post" action="">
 			<div class="form-group corpo">
-				<label for="email">Digite seu E-mail</label>
-				<input type="email" class="form-control" id="email" name="email" aria-describedby="emailHelp" placeholder="Digite seu E-mail">	
-				 <small id="emailHelp" class="form-text text-muted">Você vai receber uma senha provisória</small>
+				<label for="senha">Sua nova senha</label>
+				<input type="password" class="form-control" id="senha" name="senha" placeholder="Digite sua nova senha">
+
+				<label for="redefSenha" class="mt-3">Confirme sua senha</label>
+				<input type="password" class="form-control" id="redefSenha" name="redefSenha" placeholder="Confirme sua senha">
 			</div>
-			<button type="submit" name="btnRedefinir" class="btn btn-primary">Enviar</button>
+			<button type="submit" name="redefinir" class="btn btn-primary">Redefinir Senha</button>
 		</form>
 	</div>
+
 
 	<!-- BOOSTRAP -->
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
